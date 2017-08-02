d9bf32d37c4d607b76391c35239357a8b61ccbdc
Brian Schmalz <bschmalz@Brians-MacBook-Air.local>
2017-08-02 10:44:17 -0700

    Added debugId addresses to our parsed DOM.

--- src/dom-parse.js
+++ src/dom-parse.js
@@ -1,3 +1,6 @@
+var sinon = require('sinon'); 
+var testSpy = sinon.spy();
+
 function cloneDeep(value) {
 	if (!(value instanceof Object)) return value; 
 	const result = new value.constructor; 
@@ -13,10 +16,8 @@ function cloneDeep(value) {
     return result;
 }
 
-let iterableId = 0;
-
 const parentTraverse = (dom) => {
-  console.log('DOM', dom);
+  
   const data = {
     attributes: {},
   };
@@ -26,17 +27,14 @@ const parentTraverse = (dom) => {
   data.attributes.component = true;
   data.attributes.state = dom.state;
   data.children = [];
-  
-  // assign property as a flag for spy
-  // TODO - add conditional to not add spyID if it already exists
-  dom._instance = { _spyID: iterableId };
-  iterableId += 1;
+  data.debugId = -1; 
 
   // make call to another function where it will traverse through children
   const children = dom._reactInternalInstance._renderedComponent._renderedChildren;
   Object.values(children).forEach((child) => {
     data.children.push(traverse(child));
   });
+  console.log('PARSED DATA', data);
   return data;
 };
 
@@ -46,14 +44,11 @@ const traverse = (child) => {
     children: [],
   };
   let children;
-  var props; 
+  var props;
+  childData.debugId = child._debugID; 
   // set conditional for component vs not
   if (child.constructor.name === 'ReactCompositeComponentWrapper') {
     childData.name = child._currentElement.type.name;
-    console.log('this is child', child)
-    child._instance._spyID = iterableId;
-    iterableId += 1;
-
     childData.attributes.component = true;
     childData.attributes.state = cloneDeep(child._instance.state);
     childData.attributes.props = cloneDeep(child._instance.props);
@@ -62,6 +57,7 @@ const traverse = (child) => {
     childData.name = child._currentElement.type;
     childData.attributes.component = false;
     childData.attributes.state = null;
+
     // To revise later
     childData.attributes.props = null;
     children = child._renderedChildren;
