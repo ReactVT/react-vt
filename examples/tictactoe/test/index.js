64fee45b5a57112400cb88a9b9f1a18cac534edb
icrustandi <icrustandi@gmail.com>
2017-08-22 23:28:10 -0700

    Added images for documentation

--- examples/tictactoe/test/index.js
+++ examples/tictactoe/test/index.js
@@ -4,7 +4,7 @@ require('babel-register')({
   presets: ['es2015', 'react'],
 });
 
-require('./js/unit');
-require('./js/supertest');
-require('./js/zombie');
-require('./js/enzyme');
+// require('./js/unit');
+// require('./js/supertest');
+// require('./js/zombie');
+require('./js/enzymeTest');
