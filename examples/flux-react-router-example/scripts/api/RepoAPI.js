49e571aeacb25bfec8ad20b6d8f36dd60f9f4d5f
Ian Ramos <i.am.ian.ramos@gmail.com>
2017-08-28 13:10:37 -0700

    updated version with test suite to reflect current state of application

--- examples/flux-react-router-example/scripts/api/RepoAPI.js
+++
@@ -1,9 +0,0 @@
-import { fetchRepo, fetchRepoArray } from '../utils/APIUtils';
-
-export function getRepo(fullName, url = `repos/${fullName}`) {
-  return fetchRepo(url);
-}
-
-export function getStarredReposPage(login, url = `users/${login}/starred`) {
-  return fetchRepoArray(url);
-}
--- examples/working redux - WINDOWS/client/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import posts from './posts';
-import comments from './comments';
-
-const rootReducer = combineReducers({posts, comments, routing: routerReducer });
-
-export default rootReducer;
